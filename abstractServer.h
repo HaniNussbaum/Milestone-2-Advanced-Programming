db2a3d0b50a7d633de945beb1a659180b6f9e9dd
HaniNussbaum <haninus106@gmail.com>
2020-01-15 11:27:29 +0200

    new change

--- abstractServer.h
+++ abstractServer.h
@@ -5,4 +5,11 @@
 #ifndef MILESTONE_2_ADVANCED_PROGRAMMING_ABSTRACTSERVER_H
 #define MILESTONE_2_ADVANCED_PROGRAMMING_ABSTRACTSERVER_H
 
+class abstractServer{
+public:
+    virtual void open(int num,clientHandler handler)=0;
+    virtual void stop()=0;
+private:
+};
+
 #endif //MILESTONE_2_ADVANCED_PROGRAMMING_ABSTRACTSERVER_H
