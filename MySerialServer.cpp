3f7992c0b1bee55f45b09ee7bfa4217022e905de
HaniNussbaum <haninus106@gmail.com>
2020-01-16 13:51:19 +0200

    MySerialServer update

--- MySerialServer.cpp
+++ MySerialServer.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <netinet/in.h>
 #include "MySerialServer.h"
+#include <thread>
 
 void MySerialServer::close() {
 
@@ -17,7 +18,10 @@ void MySerialServer::open(int a_port, clientHandler a_handler) {
     this->port = port;
     this->handler = handler;
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-this->socket_num=socketfd;
+    struct timeval tv;
+    tv.tv_sec = 120;
+    setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv);
+    this->socket_num = socketfd;
     if (socketfd == -1) {
         std::cerr << "Could not create a socket" << std::endl;
         exit(-1);
@@ -30,15 +34,18 @@ this->socket_num=socketfd;
         std::cerr << "Could not bind the socket to an IP" << std::endl;
         exit(-2);
     }
+    if (listen(socket_num, 5) == -1) {
+        std::cerr << "Error during listening" << std::endl;
+        exit(-3);
+    }
+    std::thread acceptThread;
+    acceptThread = std::thread(&MySerialServer::accept, this);
 
 }
 
 void MySerialServer::accept(clientHandler handler) {
     while (true) {
-        if (listen(socket_num, 5) == -1) {
 
-        }
     }
 }
 
-#include "MySerialServer.h"
