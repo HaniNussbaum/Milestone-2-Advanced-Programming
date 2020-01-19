25fa587b339aa3ef35d2636ceeb84d7dd5c114e1
HaniNussbaum <haninus106@gmail.com>
2020-01-19 10:46:04 +0200

    pushing with my server changes and string reverser

--- MyClientHandler.h
+++ MyClientHandler.h
@@ -5,6 +5,7 @@
 #ifndef MILESTONE_2_ADVANCED_PROGRAMMING__MYCLIENTHANDLER_H_
 #define MILESTONE_2_ADVANCED_PROGRAMMING__MYCLIENTHANDLER_H_
 #include <string>
+#include <sstream>
 #include <sys/socket.h>
 #include <iostream>
 #include <netinet/in.h>
@@ -17,10 +18,10 @@
 using namespace std;
 
 class MyClientHandler : public ClientHandler {
-  Solver solver;
+  Solver<, string> solver;
   CacheManager<string>* cache_manager;
   void handleClient(int socket);
-  MyClientHandler(Solver s) {
+  MyClientHandler(Solver<string, string> s) {
     this->solver = s;
     this->cache_manager = new CacheManager<string>(MAX_CACHE);
   }
---
+++ MyTestClientHandler.h
@@ -0,0 +1,33 @@
+//
+// Created by noa on 19/01/2020.
+//
+
+#ifndef MILESTONE_2_ADVANCED_PROGRAMMING__MYTESTCLIENTHANDLER_H_
+#define MILESTONE_2_ADVANCED_PROGRAMMING__MYTESTCLIENTHANDLER_H_
+#include "ClientHandler.h"
+#include "Solver.h"
+#include "ex2.h"
+#include <sstream>
+#include <sys/socket.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+#define MAX_CACHE 100
+
+using namespace std;
+
+class MyTestClientHandler : public ClientHandler {
+  Solver<string, string> solver;
+  CacheManager<string>* cache_manager;
+  void handleClient(int socket);
+  MyTestClientHandler(Solver<string, string> s) {
+    this->solver = s;
+    this->cache_manager = new CacheManager<string>(MAX_CACHE);
+  }
+  ~MyTestClientHandler() {
+    delete this->cache_manager;
+  }
+};
+
+#endif //MILESTONE_2_ADVANCED_PROGRAMMING__MYTESTCLIENTHANDLER_H_
